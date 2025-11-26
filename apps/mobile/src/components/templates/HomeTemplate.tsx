5aac8576eeacaec1ec840d599a3a460fc36cd59c
ColeZer8 <cole.elzer@gmail.com>
2025-11-25 23:04:04 -0600

    feat(mobile): add HomeHeader component to HomeTemplate for improved UI structure

--- apps/mobile/src/components/templates/HomeTemplate.tsx
+++ apps/mobile/src/components/templates/HomeTemplate.tsx
@@ -1,6 +1,7 @@
 
 import { View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { HomeHeader } from '../organisms/HomeHeader';
 import { DailyBrief } from '../organisms/DailyBrief';
 import { ScheduleList } from '../organisms/ScheduleList';
 import { PendingActions } from '../organisms/PendingActions';
@@ -12,6 +13,7 @@ export const HomeTemplate = () => {
     return (
         <View className="flex-1 bg-white">
             <View className="flex-1 px-6" style={{ paddingTop: insets.top }}>
+                <HomeHeader />
                 <DailyBrief />
                 <ScheduleList />
                 <PendingActions />
